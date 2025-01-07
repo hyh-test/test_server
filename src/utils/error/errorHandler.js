f3f3a305e673b531c61d3642f35b73dd452fbd7e
황종하 <whdgk3615@naver.com>
2025-01-07 15:21:59 +0900

    [add]db
    db 연동

--- src/utils/error/errorHandler.js
+++ src/utils/error/errorHandler.js
@@ -1,16 +1,15 @@
 import { createResponse } from "../response/createResponse.js";
-import { ErrorCodes } from "./errorCodes.js";
 
 export const handleError = (socket, error) => {
   let responseCode;
   let message;
-  console.log(error);
+
   if (error.code) {
     responseCode = error.code;
     message = error.message;
     console.error(`에러 코드: ${error.code}, 메시지: ${error.message}`);
   } else {
-    responseCode = ErrorCodes.SOCKET_ERROR;
+    responseCode = 10000; // 일반 에러 코드
     message = error.message;
     console.error(`일반 에러: ${error.message}`);
   }
